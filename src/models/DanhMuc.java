6e31e1b869b0ec533e726b5546c49ac724807176
PianDV <khdng0424@gmail.com>
2016-05-26 11:22:14 +0700

    fix code style

--- src/models/DanhMuc.java
+++ src/models/DanhMuc.java
@@ -6,33 +6,39 @@ public class DanhMuc {
 	private String link;
 	private int super_id;
 	
-	public DanhMuc(){}
+	public DanhMuc() {
+		
+	}
 	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getLink() {
 		return link;
 	}
+	
 	public void setLink(String link) {
 		this.link = link;
 	}
+	
 	public int getSuper_id() {
 		return super_id;
 	}
+	
 	public void setSuper_id(int super_id) {
 		this.super_id = super_id;
 	}
-	
-	
-
 }
--- src/models/SanPham.java
+++ src/models/SanPham.java
@@ -8,46 +8,55 @@ public class SanPham {
 	private String detail;
 	private int super_id;
 	
-	 public SanPham(){}
-	
+	public SanPham(){
+		
+	}
 	
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
+	
 	public String getImage() {
 		return image;
 	}
+	
 	public void setImage(String image) {
 		this.image = image;
 	}
+	
 	public int getProminent() {
 		return prominent;
 	}
+	
 	public void setProminent(int prominent) {
 		this.prominent = prominent;
 	}
+	
 	public String getDetail() {
 		return detail;
 	}
+	
 	public void setDetail(String detail) {
 		this.detail = detail;
 	}
+	
 	public int getSuper_id() {
 		return super_id;
 	}
+	
 	public void setSuper_id(int super_id) {
 		this.super_id = super_id;
 	}
-	
-	
-
 }
